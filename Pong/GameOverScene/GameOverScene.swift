82f8c977903510ebb844fc35653a247f4836009e
Alberto <aldo10012@gmail.com>
2021-11-10 15:21:49 -0800

    added functionality to gameover play again btn

--- Pong/GameOverScene/GameOverScene.swift
+++ Pong/GameOverScene/GameOverScene.swift
@@ -9,16 +9,25 @@ import Foundation
 import SpriteKit
 import GameplayKit
 
+class SKButtonLabel: SKLabelNode {
+    var touchupInside: () -> Void = {}
+}
+
+
 class GameOverScene: SKScene {
     
     // MARK: Peoperties
-    
-
+    var mainLabel = SKLabelNode()
+    var playAgain = SKButtonLabel()
+    var exitGame  = SKButtonLabel()
     
     
     // MARK: Life Cycle
     override func didMove(to view: SKView) {
         print("game over")
+        mainLabel = self.childNode(withName: "mainLabel") as! SKLabelNode
+        
+        setupButtons()
     }
     
     
@@ -33,12 +42,53 @@ class GameOverScene: SKScene {
 extension GameOverScene {
     /// when user touches screen
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-                
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
         
+        buttonTap(at: location)
     }
     
-    /// when user touches and moves finger (drags)
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    func buttonTap(at location: CGPoint) {
+        let node = atPoint(location)
         
+        if let button = node as? SKButtonLabel {
+            button.touchupInside()
+        }
+    }
+    
+}
+
+// MARK: set up buttons
+extension GameOverScene {
+    func setupButtons() {
+        playAgain.text = "Play Again"
+        playAgain.fontSize = 45
+        playAgain.fontName = "Helvetica Neue Light"
+        exitGame.text = "Exit Game"
+        exitGame.fontSize = 45
+        exitGame.fontName = "Helvetica Neue Light"
+        
+        self.addChild(playAgain)
+        self.addChild(exitGame)
+        
+        playAgain.position = CGPoint(x: 0, y: -150)
+        exitGame.position = CGPoint(x: 0, y: -250)
+        
+        setupButtonActions()
+    }
+    
+    func setupButtonActions() {
+        playAgain.touchupInside = {
+            let gameOverScene = SKScene(fileNamed: "GameScene")
+            gameOverScene?.scaleMode = .aspectFill
+            self.view?.presentScene(gameOverScene)
+        }
+        
+        exitGame.touchupInside = {
+            print("this leads to home page.")
+//            let gameOverScene = SKScene(fileNamed: "GameScene")
+//            gameOverScene?.scaleMode = .aspectFill
+//            self.view?.presentScene(gameOverScene)
+        }
     }
 }
